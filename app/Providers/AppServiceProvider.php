b31324706b6e757612379997bd383b1d0f031d32
ISAAC DESMOND NRUEM <isaacebube16@gmail.com>
2025-02-23 19:48:12 +0000

    AWS compliant
    AWS test1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\member_joins;
-use App\Models\team;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Blade;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,18 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Blade::if('created', function () {
-            return team::where('user_id', Auth::id())->exists();
-        });
-
-        Blade::if('team_manager', function ($team_id) {
-            $team = Team::find($team_id);
-            return $team && $team->user_id === Auth::id();
-        });
-        
-        Blade::if('team_member', function ($user_id) {
-            $request = member_joins::find('user_id',$user_id);
-            return $request && $request->status === 'member';
-        });
+        //
     }
 }
