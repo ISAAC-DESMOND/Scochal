e8a7e696b2c2a4f25a169cc455bbcdf641186e5b
ISAAC-DESMOND <162146285+ISAAC-DESMOND@users.noreply.github.com>
2025-05-01 20:08:01 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,17 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Blade::if('created',function($user_id){
+            return team::where('user_id',Auth::id())->exists();
+        });
+        Blade::if('team_manager',function($team_id){
+            $team=team::find($team_id);
+            return $team->user_id == Auth::id();
+        });
+        Blade::if('team_member',function($team_id){
+            return member_joins::where('team_id',$team_id)->
+                where('user_id',Auth::id())->
+                where('status','member')->exists();
+        });
     }
 }
