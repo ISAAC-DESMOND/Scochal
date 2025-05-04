e867b8923693ecdd648c57fa9101595e0d182ac4
ISAAC-DESMOND <162146285+ISAAC-DESMOND@users.noreply.github.com>
2025-05-04 15:05:29 +0100

    Update matches.blade.php

--- resources/views/matches/matches.blade.php
+++ resources/views/matches/matches.blade.php
@@ -42,32 +42,55 @@
                     match_div.innerHTML =`
                     @foreach($matches as $match)
                         <li class="bg-white dark:bg-gray-800 shadow w-full mx-auto py-6 px-4 sm:px-6 lg:px-8 rounded-md font-semibold text-l text-gray-800 dark:text-gray-200 leading-tight hover:scale-120 transition-transform duration-300 ease-in-out" style="display:flex;flex-direction:row;margin:0.2em;">
-                            <form action="{{route('team.join')}}" method="POST">
+                                                        <form action="{{route('inbox.create',[$match->home->user_id])}}" method="GET">
                                     @csrf
-                                    <input type="hidden" id="team_id" name="team_id" value="{{ $match->home_team_id ?? '' }}">
+                                    <input type="hidden" id="user_id" name="user_id" value="{{ $match->home->user_id ?? '' }}">
+                                    @team_manager($match->away_team_id)
                                     <x-primary-button class="mt-1 justify-center px-8" id="submit_btn">
-                                        {{ __('Join Team') }}
+                                        {{ __('Message Manager') }}
                                     </x-primary-button>
+                                    @endteam_manager
                                 </form>
+                            <form action="{{route('match.edit',[$match->id])}}" method="GET">
+                                    @csrf
+                                    <input type="hidden" id="match_id" name="match_id" value="{{ $match->id ?? '' }}">
+                                    @team_manager($match->home_team_id)
+                                    <x-primary-button class="mt-1 justify-center px-8" id="submit_btn">
+                                        {{ __('Edit Match') }}
+                                    </x-primary-button>
+                                    @endteam_manager
+                                </form>
+
                                 <div class="text-center" style="margin-left: auto;">
                                     <div>
-                                        {{$match->home->name}}
+                                        {{$match->home->name}} [{{$match->home_team_score}}]
                                         :
-                                        {{$match->away->name}}
+                                         [{{$match->away_team_score}}] {{$match->away->name}}
                                     </div>
                                     <div>
-                                    {{ $match->match_date->format('F j, Y') }}
+                                    {{ $match->match_date->format('F j, Y') }} ,
                                      {{ $match->match_date->format('g:i A') }}
                                     </div>
-                                    {{$match->court_location}}
+                                    {{$match->court_location}} , {{$match->home->description}}
                                 </div>
-                            <form action="{{route('team.join')}}" method="POST" style="justify-self:end;margin-left: auto;>
+                            <form action="{{route('inbox.create',[$match->away->user_id])}}" method="GET" style="justify-self:end;margin-left: auto;>
                                 @csrf
-                                <input type="hidden" id="team_id" name="team_id" value="{{ $match->away_team_id ?? '' }}">
+                                <input type="hidden" id="user_id" name="user_id" value="{{ $match->away->user_id ?? '' }}">
+                                @team_manager($match->home_team_id)
                                 <x-primary-button class="mt-1 justify-center px-8" id="submit_btn" style="justify-self:end;margin-left: auto;">
-                                    {{ __('Join Team') }}
+                                    {{ __('Message Manager') }}
                                 </x-primary-button>
+                                @endteam_manager
                             </form>
+                            <form action="{{route('match.edit',[$match->id])}}" method="GET">
+                                    @csrf
+                                    <input type="hidden" id="match_id" name="match_id" value="{{ $match->id ?? '' }}">
+                                    @team_manager($match->away_team_id)
+                                    <x-primary-button class="mt-1 justify-center px-8" id="submit_btn">
+                                        {{ __('Edit Match') }}
+                                    </x-primary-button>
+                                    @endteam_manager
+                                </form>
                         </li>
                     @endforeach`;
 
